08138862a22b543639f4b31e36a94ded813e4cd7
sylasthekingslayer <sylasthekingslayer@proton.me>
2025-03-26 03:35:46 +0300

    imagepng

--- docs/.vitepress/theme/components/RandomContent.vue
+++ docs/.vitepress/theme/components/RandomContent.vue
@@ -11,7 +11,7 @@ const quotesList = [
 ]
 
 const imagesList = [
-  "/images/1.jpg", "/images/2.jpg", "/images/3.jpg", "/images/4.jpg", "/images/5.jpg", 
+  "/images/1.jpg", "/images/2.jpg", "/images/3.png", "/images/4.jpg", "/images/5.jpg", 
   "/images/6.jpg", "/images/7.jpg", "/images/8.jpg", "/images/9.jpg", "/images/10.jpg", 
   "/images/11.jpg", "/images/12.jpg", "/images/13.jpg", "/images/14.jpg"
 ]
